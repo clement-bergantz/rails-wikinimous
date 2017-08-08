b299f33ffa73ad31e3f1851c41e525e23d03f6d3
clement bergantz <bergantz.clement@gmail.com>
2017-08-08 21:34:24 +0200

    added index, show, new, create, edit, update, destroy methods in controller + some refacto

---
+++ app/controllers/recipes_controller.rb
@@ -0,0 +1,52 @@
+class RecipesController < ApplicationController
+  before_action :find_recipe, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @recipes = Recipe.all
+  end
+
+  def show
+
+  end
+
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    @recipe = Recipe.new(recipe_params)
+    if @recipe.save
+      redirect_to recipes_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+
+  end
+
+  def update
+    if @recipe.update(recipe_params)
+      redirect_to recipes_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @recipe.destroy
+    redirect_to recipes_path
+  end
+
+end
+
+private
+
+def find_recipe
+  @recipe = Recipe.find(params[:id])
+end
+
+def recipe_params
+  params.require(:recipe).permit(:name, :ingredient, :create_at)
+end
